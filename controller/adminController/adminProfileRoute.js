18bf86c95969910d24f38a702475a93413eeb68c
Mario-E-G <marioehab330@gmail.com>
2023-03-04 15:06:18 +0200

    admin routes

--- controller/adminController/adminProfileRoute.js
+++ controller/adminController/adminProfileRoute.js
@@ -9,6 +9,8 @@ const adminHomeRoute = async (req, res) => {
     return res.send("Admin Home Page")
 }
 
+
+///////////////////////////////////GetAll//////////////////////////////////////
 // get all categories
 const adminGetAllCategory = async (req, res) => {
     try {
@@ -23,7 +25,6 @@ const adminGetAllCategory = async (req, res) => {
     }
 
 }
-
 // get all books
 const adminGetAllBook = async (req, res) => {
     try {
@@ -37,7 +38,6 @@ const adminGetAllBook = async (req, res) => {
         return res.status(500).send({ Message: error.message });
     }
 }
-
 // get all authors
 const adminGetallAuthor = async (req, res) => {
     try {
@@ -53,6 +53,51 @@ const adminGetallAuthor = async (req, res) => {
 }
 
 
+
+//////////////////////////////////////getById//////////////////////////////////
+// get category by id
+const adminGetCategoryById = async (req, res) => {
+    try {
+        const category = await categoryModel.findById(req.params.id);
+        if (category) {
+            return res.status(200).send(category);
+        } else {
+            return res.status(404).send({ Message: "Category Not Found" });
+        }
+    } catch (error) {
+        return res.status(500).send({ Message: "Error In Retrieval" });
+    }
+}
+// get book by id
+const adminGetBookById = async (req, res) => {
+    try {
+        const book = await bookModel.findById(req.params.id);
+        if (book) {
+            return res.status(200).send(book);
+        } else {
+            return res.status(404).send({ Message: "Book Not Found" });
+        }
+    } catch (error) {
+        return res.status(500).send({ Message: "Error In Retrieval" });
+    }
+}
+// get author by id
+const adminGetAuthorById = async (req, res) => {
+    try {
+        const author = await authorModel.findById(req.params.id);
+        if (author) {
+            return res.status(200).send(author);
+        } else {
+            return res.status(404).send({ Message: "Author Not Found" });
+        }
+    } catch (error) {
+        return res.status(500).send({ Message: "Error In Retrieval" });
+    }
+}
+
+
+
+//////////////////////////////////////Add////////////////////////////////////////
 //add new Category
 const adminAddNewCategory = async (req, res) => {
     const newCategory = { ...req.body };
@@ -63,7 +108,6 @@ const adminAddNewCategory = async (req, res) => {
         return res.status(500).send({ Message: error.message });
     }
 }
-
 //add new book
 const adminAddNewBook = async (req, res) => {
     try {
@@ -74,7 +118,6 @@ const adminAddNewBook = async (req, res) => {
         return res.status(500).send({ Message: error.message });
     }
 }
-
 //add new author
 const adminAddNewAuthor = async (req, res) => {
     try {
@@ -86,14 +129,99 @@ const adminAddNewAuthor = async (req, res) => {
     }
 }
 
+
+
+/////////////////////////////////////Update///////////////////////////////////////
 //update category
 const adminUpdateCategory = async (req, res) => {
+    const updatedCategory = { ...req.body };
     try {
-        const updatedCategory = { ...req.body };
-        const category = categoryModel.findByIdAndUpdate(req.params.id, updatedCategory);
-        return res.status(202).json(updatedCategory);
+        const category = await categoryModel.findByIdAndUpdate(req.params.id, updatedCategory, { new: true });
+        if (category) {
+            return res.status(200).send({ Message: "Updated Successfully" });
+        } else {
+            return res.status(404).json({ Message: "Category isn't Exist" });
+        }
     } catch (error) {
-        return res.status(500).send({ Message: error.message });
+        return res.status(500).send({ Message: "Error in Update" });
+    }
+}
+//update book
+const adminUpdateBook = async (req, res) => {
+    const bookData = { ...req.body };
+    try {
+        const book = await bookModel.findByIdAndUpdate(req.params.id, bookData, { new: true });
+        if (book) {
+            return res.status(200).send({ Message: "Updated Successfully" });
+        } else {
+            return res.status(404).json({ Message: "Book is not Exist" });
+        }
+    } catch (error) {
+        return res.status(500).json({ Message: "Error In Update" });
+
+    }
+}
+//update author
+const adminUpdateAuthor = async (req, res) => {
+    const authorData = { ...req.body };
+    try {
+        const author = await authorModel.findByIdAndUpdate(req.params.id, authorData, { new: true });
+        if (author) {
+            return res.status(200).send({ Message: "Updated Successfully" });
+        } else {
+            return res.status(404).send({ Message: "Author is not Exist" });
+        }
+    } catch (error) {
+        return res.status(500).json({ Message: "Error In Update" });
+
+    }
+}
+
+
+
+
+
+////////////////////////////////////Delete////////////////////////////////////////
+//delete category
+const adminDeleteCategory = async (req, res) => {
+    try {
+        const deletedCategory = await categoryModel.findByIdAndDelete(req.params.id);
+        if (deletedCategory) {
+            return res.status(200).send({ Message: "Deleted Successfully" });
+        } else {
+            return res.status(404).send({ Message: "category is not Exist" });
+        }
+    } catch (error) {
+        return res.status(500).json({ Message: "Error In Delete" });
+
+    }
+}
+//delete book
+const adminDeleteBook = async (req, res) => {
+    try {
+        const deletedBook = await bookModel.findByIdAndDelete(req.params.id);
+        if (deletedBook) {
+            return res.status(200).send({ Message: "Deleted Successfully" });
+        } else {
+            return res.status(404).send({ Message: "book is not Exist" });
+        }
+    } catch (error) {
+        return res.status(500).json({ Message: "Error In Delete" });
+
+    }
+}
+//delete author
+const adminDeleteAuthor = async (req, res) => {
+    try {
+        const deletedAuthor = await authorModel.findByIdAndDelete(req.params.id);
+        if (deletedAuthor) {
+            return res.status(200).send({ Message: "Deleted Successfully" });
+        } else {
+            return res.status(404).send({ Message: "Author is not Exist" });
+        }
+    } catch (error) {
+        return res.status(500).json({ Message: "Error In Delete" });
+
     }
 }
 
@@ -106,4 +234,14 @@ module.exports = {
     adminAddNewCategory,
     adminAddNewBook,
     adminAddNewAuthor,
+    adminUpdateCategory,
+    adminUpdateBook,
+    adminUpdateAuthor,
+    adminDeleteCategory,
+    adminDeleteBook,
+    adminDeleteAuthor,
+    adminGetCategoryById,
+    adminGetBookById,
+    adminGetAuthorById,
+
 }
--- route/adminRoutes/adminRoute.js
+++ route/adminRoutes/adminRoute.js
@@ -6,17 +6,34 @@ const {
     adminGetAllBook,
     adminGetallAuthor,
     adminAddNewBook,
-    adminAddNewAuthor } = require("../../controller/adminController/adminProfileRoute");
+    adminAddNewAuthor,
+    adminUpdateCategory,
+    adminUpdateBook,
+    adminUpdateAuthor,
+    adminDeleteCategory,
+    adminDeleteBook,
+    adminDeleteAuthor,
+    adminGetCategoryById,
+    adminGetBookById,
+    adminGetAuthorById } = require("../../controller/adminController/adminProfileRoute");
 const router = express.Router();
 
 
 //admin-profile
 router.get("/", adminHomeRoute);
 
+
 //get-all
 router.get("/category", adminGetAllCategory);
 router.get("/book", adminGetAllBook);
 router.get("/author", adminGetallAuthor);
+
+
+//add-new(category-book-author)
+router.get("/category/:id", adminGetCategoryById);
+router.get("/book/:id", adminGetBookById);
+router.get("/author/:id", adminGetAuthorById);
+
 
 //add-new(category-book-author)
 router.post("/category", adminAddNewCategory);
@@ -24,8 +41,15 @@ router.post("/book", adminAddNewBook);
 router.post("/author", adminAddNewAuthor);
 
 
+//update(category-book-author)
+router.patch("/category/:id", adminUpdateCategory);
+router.patch("/book/:id", adminUpdateBook);
+router.patch("/author/:id", adminUpdateAuthor);
 
 
-
+//delete(category-book-author)
+router.delete("/category/:id", adminDeleteCategory);
+router.delete("/book/:id", adminDeleteBook);
+router.delete("/author/:id", adminDeleteAuthor);
 
 module.exports = router;
