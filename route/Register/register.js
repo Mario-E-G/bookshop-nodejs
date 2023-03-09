af159b9b84bb9c1c28be7809f6742f0920efdc99
Mario-E-G <marioehab330@gmail.com>
2023-03-09 15:34:55 +0200

    middle ware

--- route/Register/register.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerAuth } = require("../../controller/auth/registerAuth");
-
-router.post("/register", registerAuth);
-
-module.exports = router;
--- route/logIn/logIn.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router()
-const { loginAuth } = require("../../controller/auth/logInAuth");
-
-router.post("/", loginAuth);
-
-module.exports = router;
-
